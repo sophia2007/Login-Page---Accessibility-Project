1a9e8bd701a1d09bf327d2fe35a799ece42166bf
sophia2007 <62575423+sophia2007@users.noreply.github.com>
2024-11-19 22:46:46 -0500

    final version

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const signupForm = document.querySelector("#signup");
-    const resetPWForm = document.querySelector("#resetPW");
 
     document.querySelector("#linkSignup").addEventListener("click", () => {
         loginForm.classList.add("form--hidden");
         signupForm.classList.remove("form--hidden");
-        resetPWForm.classList.add("form--hidden");
     });
 
-    document.querySelector("#linkResetPW").addEventListener("click", () => {
-        loginForm.classList.add("form--hidden");
-        signupForm.classList.add("form--hidden");
-        resetPWForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin1").addEventListener("click", () => {
+    document.querySelector("#linkLogin").addEventListener("click", () => {
         loginForm.classList.remove("form--hidden");
         signupForm.classList.add("form--hidden");
-        resetPWForm.classList.add("form--hidden");
     });
 
-    document.querySelector("#linkLogin2").addEventListener("click", () => {
-        loginForm.classList.remove("form--hidden");
-        signupForm.classList.add("form--hidden");
-        resetPWForm.classList.add("form--hidden");
-    });
 });
+
+
 
 /*===== FOCUS =====*/
 // creates the animation where the text floats up
